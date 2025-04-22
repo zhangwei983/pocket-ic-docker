69d14f90294a14cec595988e8434c4b100fd8692
Vincent Zhang <zhangwei983@hotmail.com>
2025-04-22 23:48:17 +0800

    Bind the ip address to 0.0.0.0, and use PocketIcBuilder to create an instance on the server.

--- pocket-ic-test/src/main.rs
+++ pocket-ic-test/src/main.rs
@@ -4,15 +4,14 @@ fn main() {
 
 #[test]
 fn test_pocket_ic() {
-    use pocket_ic::PocketIc;
+    use pocket_ic::PocketIcBuilder;
 
-    let pocket_ic = PocketIc::new_from_existing_instance(
-        url::Url::parse("http://localhost:8081").unwrap(),
-        1,
-        None,
-    );
-    println!("pocket ic id: {:?}", pocket_ic.instance_id());
+    let pic = PocketIcBuilder::new()
+        .with_server_url(url::Url::parse("http://172.31.224.1:8081").unwrap())
+        .with_application_subnet()
+        .build();
+    println!("pocket ic id: {:?}", pic.instance_id());
 
-    let canister_id = pocket_ic.create_canister();
+    let canister_id = pic.create_canister();
     println!("canister id: {:?}", canister_id);
 }
